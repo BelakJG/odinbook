872a70959e7a26cb4c02ec60a18cb5a623c9f56e
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-19 12:47:18 -0400

    Added user reference as author to posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  belongs_to :author, class_name: "User"
   has_many :comments, as: :commentable, dependent: :destroy
 end
