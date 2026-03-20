42064c7c87b60e4e2a77243c7952dff7d28f8fc1
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-20 11:20:19 -0400

    Added follow list validations

--- app/models/follow_list.rb
+++ app/models/follow_list.rb
@@ -1,4 +1,7 @@
 class FollowList < ApplicationRecord
+  validates :user_id, presence: true
+  validates :following_id, presence: true
+
   belongs_to :user
   belongs_to :following, class_name: "User"
 end
