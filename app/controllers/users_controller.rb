43b737f64737af11467ea0ab9582718090e0b86e
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-25 14:35:48 -0400

    Created index for users and created user partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
